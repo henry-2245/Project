ef67ff8d9f6a83542284bf54abf5617aa38fb05a
Chayut Anegsiripong <chayut_henry@hotmail.com>
2022-05-27 21:12:01 +0700

    update shop_page

--- Project_code/shop_page.py
+++ Project_code/shop_page.py
@@ -6,15 +6,20 @@ from PySide6.QtWidgets import * # must need 3 of these lines ***
 from PySide6.QtCore import *
 from PySide6.QtGui import *
 import shop_page_win
+import basket_page_win
 
 class ShopPage(QDialog):
     
-    def __init__(self):
+    def __init__(self, item_name, item_price):
         
         QDialog.__init__(self, None)
         self.ui = shop_page_win.Ui_Dialog()
         self.ui.setupUi(self)
         self.setWindowTitle("Terbal Shop Page")
+        
+        # items name and price
+        self.item_name = item_name
+        self.item_price = item_price
 
         # Basket button
         self.item_num = 0
@@ -27,13 +32,10 @@ class ShopPage(QDialog):
         # middle layout
         layout_middle = QVBoxLayout()
         
-        # items name and price
-        self.item_name = ["อ้วยอันโอสถ \nยาแคปซูลฟ้าทะลายโจรสกัด", "ยันฮี ยาฟาร์แท็บ ฟ้าทะลายโจร", "Ya Dom Poi-Sian", "Propoliz mouth spray"]
-        self.item_price = [120, 80, 20, 90]
-        
         self.amount_of_item = len(self.item_name)
         self.item_quantity = []
         self.orderButton = []
+        
         self.item_quantity_label = []
         item_name_label = []
         item_price_label = []
@@ -70,9 +72,12 @@ class ShopPage(QDialog):
         self.orderButton[3].clicked.connect(lambda: self.gotoSelectItem(3))
         
         print("Program running!!")
-        self.displayInShop = False
+        # basket button
+        self.ui.basketButton.clicked.connect(self.gotoBasketPage)
+        
         self.ui.scrollAreaWidgetContents.setLayout(layout_middle) # scrollAreaWidgetContents is in scrollArea (they are created together)
-    
+        
+        
     def displayInShopPage(self, i, text):
         if text == "add":
             self.item_quantity[i] += 1
@@ -83,10 +88,7 @@ class ShopPage(QDialog):
                 self.item_quantity[i] -= 1
                 self.item_num -= 1 # for all items at basket button
                 self.net_price -= self.item_price[i]
-            
-            
-        
-        
+                
         print("Quantity: ", self.item_quantity[i], ", i: ", i)
         self.item_quantity_label[i].setText("x" + str(self.item_quantity[i]))
         
@@ -105,7 +107,7 @@ class ShopPage(QDialog):
         layout_control = []
         plus_button = []
         minus_button = []
-        addToBasketButton = []
+        self.addToBasketButton = []
         self.item_amount_label = []
         self.item_amount = []
         layout_big = []
@@ -135,7 +137,7 @@ class ShopPage(QDialog):
             
             self.item_amount_label.append(QLabel(self))
             addBasketButton = QPushButton(self)
-            addToBasketButton.append(addBasketButton)
+            self.addToBasketButton.append(addBasketButton)
             
         for i in range(self.amount_of_item):
             # # customize button
@@ -171,21 +173,23 @@ class ShopPage(QDialog):
             font3.setPointSize(26)
             self.item_amount_label[i].setFont(font3)
             self.item_amount_label[i].setAlignment(Qt.AlignCenter)
+            # if self.item_amount[i] == 0: 
+            #     self.item_amount[i] = 1 # if u want to start with 1
             self.item_amount_label[i].setText(str(self.item_amount[i]))
             layout_control[i].addWidget(minus_button[i])
             layout_control[i].addWidget(self.item_amount_label[i])
             layout_control[i].addWidget(plus_button[i])
             layout_big[i].addLayout(layout_control[i])
             
-            # may be cutomize addToBasketButton
+            # may be cutomize self.addToBasketButton
             font4 = QFont()
             font4.setPointSize(24)
-            addToBasketButton[i].setLayoutDirection(Qt.LeftToRight)
-            addToBasketButton[i].setStyleSheet(u"text-align: left;")
-            addToBasketButton[i].setFont(font4)
-            addToBasketButton[i].setText("Add to Basket                    ฿" + str(self.item_price[i]))
+            self.addToBasketButton[i].setLayoutDirection(Qt.LeftToRight)
+            self.addToBasketButton[i].setStyleSheet(u"text-align: left;")
+            self.addToBasketButton[i].setFont(font4)
+            self.addToBasketButton[i].setText("Add to Basket                    ฿" + str(self.item_price[i] * self.item_amount[i]))
             
-            layout_big[i].addWidget(addToBasketButton[i])
+            layout_big[i].addWidget(self.addToBasketButton[i])
 
             self.popup[i].setLayout(layout_big[i])
         
@@ -200,10 +204,10 @@ class ShopPage(QDialog):
         minus_button[2].clicked.connect(lambda: self.minusAmount(2))
         minus_button[3].clicked.connect(lambda: self.minusAmount(3))
         
-        addToBasketButton[0].clicked.connect(self.popup[0].close)
-        addToBasketButton[1].clicked.connect(self.popup[1].close)
-        addToBasketButton[2].clicked.connect(self.popup[2].close)
-        addToBasketButton[3].clicked.connect(self.popup[3].close)
+        self.addToBasketButton[0].clicked.connect(self.popup[0].close)
+        self.addToBasketButton[1].clicked.connect(self.popup[1].close)
+        self.addToBasketButton[2].clicked.connect(self.popup[2].close)
+        self.addToBasketButton[3].clicked.connect(self.popup[3].close)
         
         self.popup[index].show()
         
@@ -228,13 +232,108 @@ class ShopPage(QDialog):
     
     def displayInPopUp(self, i):
         self.item_amount_label[i].setText(str(self.item_amount[i]))
+        self.addToBasketButton[i].setText("Add to Basket                    ฿" + str(self.item_price[i] * self.item_amount[i]))
         print("Amount: ", self.item_amount[i], ", i: ", i)
+    
+    def gotoBasketPage(self):
+        print("Basket button is clicked!!")
+        
+        basketPage = BasketPage(self.item_name, self.item_price, self.item_quantity, self.net_price)
+        
+        widget.addWidget(basketPage)
+        widget.setCurrentIndex(widget.currentIndex() + 1)
+
+class BasketPage(QDialog):
+    
+    def __init__(self, item_name, item_price, item_quantity, all_food_price):
+        
+        QDialog.__init__(self, None)
+        self.ui = basket_page_win.Ui_Dialog()
+        self.ui.setupUi(self)
+        self.setWindowTitle("Basket Page")
+        self.ui.basketBackButton.clicked.connect(self.goBackToShopPage)
+        
+        # set value
+        self.item_quantity = item_quantity # list
+        self.item_name = item_name # list
+        self.item_price = item_price # list
+        self.all_food_price = all_food_price
+        
+        print("Name: ", self.item_name)
+        print("Price: ", self.item_price)
+        print("Quantity: ", self.item_quantity)
+        print("All food price: ", self.all_food_price)
+        
+        layout_middle = QVBoxLayout()
+        self.amount_of_item = len(self.item_name)
+        
+        item_quantity_label = []
+        item_name_label = []
+        item_price_label = []
+        min_layout = []
+        
+        for i in range(self.amount_of_item):
+            item_quantity_label.append(QLabel())  # quantity
+            item_name_label.append(QLabel()) # name
+            item_price_label.append(QLabel()) # price
+            min_layout.append(QHBoxLayout()) # line by line
+            
+        for i in range(self.amount_of_item):
+            item_quantity_label[i].setText("x" + str(self.item_quantity[i]))
+            item_name_label[i].setText(self.item_name[i])
+            item_price_label[i].setText(str(self.item_price[i] * self.item_quantity[i]) + "฿")
+            
+            min_layout[i].addWidget(item_quantity_label[i])
+            min_layout[i].addWidget(item_name_label[i])
+            min_layout[i].addWidget(item_price_label[i])
+            layout_middle.addLayout(min_layout[i])      
+        
+        self.ui.basketScrollAreaWidgetContents.setLayout(layout_middle)
+        
+        # set the value in UI window
+        self.deliveryFee = 20
+        self.ui.all_food_price_label.setText("฿" + str(self.all_food_price))
+        self.ui.delivery_fee_label.setText("฿" + str(self.deliveryFee))
+        
+        self.total_price = self.all_food_price + self.deliveryFee
+        self.ui.total_price_label.setText("฿" + str(self.total_price))
+        
+        self.ui.placeOrderButton.clicked.connect(self.confirmOrder)
         
         
+    # set when ผ่านหน้าเลือกร้านมา เพื่อทำให้รู้ว่าคือร้านไหน ชื่ออะไร จากการใช้ i
+    def setIndexOfShop(self, i): # ใช้ obj แล้ว call เลย
+        pass
+            
+    # def getIndexOfShop(self):
+    #     return int(self.deliveryFee)
+    
+    def confirmOrder(self):
+            
+        print("Order Confirm:")
+        print("Item name: ", self.item_name)        
+        print("Item price: ", self.item_price)
+        print("Item quantity: ", self.item_quantity)
+        print("Payment method: ", self.ui.paymentMethodComboBox.currentText())
+        
+    def goBackToShopPage(self):
+        basketPage = BasketPage(self.item_name, self.item_price, self.item_quantity, self.all_food_price)
+        
+        widget.removeWidget(basketPage)
+        widget.addWidget(shopPage)
+        widget.setCurrentIndex(widget.currentIndex() + 1)
+
+
+
+# main func
 if __name__ == "__main__":
     app = QApplication(sys.argv)
+    
+    # items name and price
+    item_name = ["อ้วยอันโอสถ \nยาแคปซูลฟ้าทะลายโจรสกัด", "ยันฮี ยาฟาร์แท็บ ฟ้าทะลายโจร", "Ya Dom Poi-Sian", "Propoliz mouth spray"]
+    item_price = [120, 80, 20, 90]
 
-    shopPage = ShopPage()
+    shopPage = ShopPage(item_name, item_price)
     shopPage.show()
     
     # just add *
